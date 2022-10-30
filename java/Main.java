d0e85343dbb18535f90bced94fa61fdedff60dc6
Angel Peraza <angelfuturos@gmail.com>
2022-10-29 19:01:38 -0500

    Aplicando encapsulamiento en java

--- java/Main.java
+++ java/Main.java
@@ -2,13 +2,14 @@ class Main {
         
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123", "email@dominio.com" ));
-        car.passenger = 4;
-        car.printDataCar();
-        
-        Car car2 = new Car("LSP12D", new Account("Angel Peraza", "LSP12D", "email@dominio.com"));
-        car2.passenger = 2;
-        car2.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123", "email@dominio.com" ), "Chevrolet","Spark");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+
+        UberVan uberVan = new UberVan("ABC556", new Account("alexis campo", "1033888", "programacion@hotmail.com"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
         
     }
 }
---
+++ python/paypal.py
@@ -0,0 +1,10 @@
+import email
+from payment import Payment
+
+email = str
+
+class Paypal(Payment):
+    def __init__(self, id, email):
+        super().__init__(id)
+        self.email = email
+        
